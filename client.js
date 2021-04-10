964390e5fef20add652dd7ec3464266d82abdfce
Shyla Earl <shyla.earl@gmail.com>
2021-04-10 11:11:54 -0500

    submit button capturing inputs and appending them to DOM

--- client.js
+++ client.js
@@ -4,22 +4,54 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('jQ sourced');
-    
+    //clicking submit button runs corresponding fucntion to 
+    //capture and add input values to the DOM
+    $("#submitInput").on('click', submitEmployeeInfo);
+}
+
+//function captures values from DOM and displays them on DOM
+function submitEmployeeInfo(){
+    //variables to hold input values from DOM
+    let firstName = $('#firstNameInput').val();
+    let lastName = $('#lastNameInput').val();
+    let id = Number($('#idInput').val());
+    let title = $('#titleInput').val();
+    let annualSalary = Number($('#annualSalaryInput').val());
+
+    //appending input values to a table/form on the DOM
+    $('.employeeInfoTable').append(
+        `<tr>
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${id}</td>
+            <td>${title}</td>
+            <td>$${annualSalary}</td>
+            <td>
+                <button class="deleteButton">Delete</button>
+            </td>
+        </tr>`
+    );
+
+    //clears inputs on DOM
+    $('#firstNameInput').val('');
+    $('#lastNameInput').val('');
+    Number($('#idInput').val(''));
+    $('#titleInput').val('');
+    Number($('#annualSalaryInput').val());
 }
 
 
 
-//The application should have an input form that collects 
-//employee first name, last name, ID number, job title, annual salary.
+//[X]create an input form that collects first name, last name, 
+//ID number, job title, annual salary.
 
-//A 'Submit' button should collect the form information, 
-//store the information to calculate monthly costs, 
-//append information to the DOM and clear the input fields. 
-//Using the stored information, calculate monthly costs and 
-//append this to the to DOM. If the total monthly cost exceeds $20,000, 
-//add a red background color to the total monthly cost.
+//[X]create a 'Submit' button to collect form information, 
+//[]store the information to calculate monthly costs, 
+//[X]append information to the DOM and clear the input fields. 
+//[]calculate monthly costs and append this to the to DOM. 
+//[]If the total monthly cost exceeds $20,000, add a red background color.
 
-//Create a delete button that removes an employee from the DOM. 
+//[X]Create a delete button that removes an employee from the DOM. 
 //For Base mode, it does not need to remove that Employee's salary 
 //from the reported total.
 
@@ -31,8 +63,3 @@ function onReady(){
 //which element was removed. You will need to use .text() as a getter 
 //or look into jQuery's .data() function. This is tricky!
 
-
-//1. You have to be able to add a new entry to your database.
-//2. Show the entries of your database.
-//3. Calculate a number based on a field of each entry.
-//4. Be able to delete entries.
