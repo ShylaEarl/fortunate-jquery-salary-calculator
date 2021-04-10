09ce317ef9f1aa111ba7fa98c4bf98b0c0b43a50
Shyla Earl <shyla.earl@gmail.com>
2021-04-10 12:06:33 -0500

    attempting calculate function with variables

--- client.js
+++ client.js
@@ -6,7 +6,8 @@ function onReady(){
     console.log('jQ sourced');
     //clicking submit button runs corresponding fucntion to 
     //capture and add input values to the DOM
-    $("#submitInput").on('click', submitEmployeeInfo);
+    $('#submitButton').on('click', submitEmployeeInfo);
+    //$('#submitButton').on('click', calculateMonthlySalary);
 }
 
 //function captures values from DOM and displays them on DOM
@@ -19,7 +20,7 @@ function submitEmployeeInfo(){
     let annualSalary = Number($('#annualSalaryInput').val());
 
     //appending input values to a table/form on the DOM
-    $('.employeeInfoTable').append(
+    $('#employeeInfoTable').append(
         `<tr>
             <td>${firstName}</td>
             <td>${lastName}</td>
@@ -32,21 +33,52 @@ function submitEmployeeInfo(){
         </tr>`
     );
 
-    //clears inputs on DOM
-    $('#firstNameInput').val('');
-    $('#lastNameInput').val('');
-    Number($('#idInput').val(''));
-    $('#titleInput').val('');
-    Number($('#annualSalaryInput').val());
+    clearInputs();
+    calculateMonthlySalary();
+}
+
+function calculateMonthlySalary(){
+    //variable to store total annual salary of all employees
+    let totalAnnualSalary = 0;
+    //variable to store total monthly salary of all employees
+    let totalMonthlySalary = 0;
+    //get values from annual salary input
+    let eachAnnualSalary = Number($('#annualSalaryInput').val());
+    //add them together and assign total to totalAnnualSalary variable
+    console.log('calculate', eachAnnualSalary);
+    totalAnnualSalary += eachAnnualSalary;
+    console.log('calculate', totalAnnualSalary);
+    //divide totalAnnualSalary by 12 to get total monthly salary costs
+    totalMonthlySalary = totalAnnualSalary / 12;
+    console.log('calculate', totalMonthlySalary);
+
+    let el = $('#totalMonthlySalary');
+    el.empty();
+    el.append(totalMonthlySalary);
+
+    if(totalMonthlySalary > 20000){
+        el.addClass('redColor');
+    }
+    
+}
+
+function clearInputs(){
+     //clears inputs on DOM
+     $('#firstNameInput').val('');
+     $('#lastNameInput').val('');
+     Number($('#idInput').val(''));
+     $('#titleInput').val('');
+     Number($('#annualSalaryInput').val(''));
 }
 
 
+////TODO/////
 
 //[X]create an input form that collects first name, last name, 
 //ID number, job title, annual salary.
 
 //[X]create a 'Submit' button to collect form information, 
-//[]store the information to calculate monthly costs, 
+//[]store the information to calculate monthly costs, (variable? or array?)
 //[X]append information to the DOM and clear the input fields. 
 //[]calculate monthly costs and append this to the to DOM. 
 //[]If the total monthly cost exceeds $20,000, add a red background color.
